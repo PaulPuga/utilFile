33a1e2c62358b99782296bac0a1e23842c2aec20
Paul Puga <puga.540.puga@gmail.com>
2023-11-06 17:22:12 -0600

    fixing cors whitelist filter

--- backend/src/config/cors.config.ts
+++ backend/src/config/cors.config.ts
@@ -1,13 +1,14 @@
 import { CorsOptions } from 'cors';
+import boom from '@hapi/boom';
 
 const whitelist = ['http://localhost:3000'];
 
 export const corsOptions: CorsOptions = {
   origin: (origin, callback) => {
-    if (whitelist.includes(origin as string)) {
-      callback(new Error('Not allowed by CORS'));
-    } else {
+    if (whitelist.includes(origin as string) || !origin) {
       callback(null, true);
+    } else {
+      callback(boom.unauthorized('Not allowed by CORS'));
     }
   },
 };
